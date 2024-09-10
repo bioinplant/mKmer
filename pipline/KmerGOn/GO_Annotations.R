ece536eebe4bb8ea8671088a44d787a90b8d1d91
mofy <141097405+MoFangFFFF@users.noreply.github.com>
2024-09-10 09:34:52 +0800

    Update GO_Annotations.R

--- pipline/KmerGOn/GO_Annotations.R
+++ pipline/KmerGOn/GO_Annotations.R
@@ -1,11 +1,9 @@
 set.seed(12345)
-# 加载必要的包
 library(optparse)
 library(xml2)
 library(dplyr)
 library(ggplot2)
 
-# 定义命令行参数
 option_list <- list(
   make_option(c("-c", "--cluster"), type = "integer", help = "Cluster number", metavar = "integer"),
   make_option(c("-o", "--out"), type = "character", help = "Output folder path", metavar = "character"),
@@ -14,18 +12,15 @@ option_list <- list(
   make_option(c("-i", "--implied_c"), type = "character", default = "n", help = "Implied condition", metavar = "character")
 )
 
-# 解析命令行参数
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
-
-# 获取参数值
 cluster_num <- opt$cluster
 out_folder <- opt$out
 xml_file <- opt$xml
 txt_file <- opt$txt
 implied_c <- opt$implied_c
 
-######################## 从gomo.xml中提取信息 ############################
+######################## Extract information from 'gomo.xml' ############################
 xml_data <- read_xml(xml_file)
 motifs <- xml_data %>% 
   xml_find_all("//motif")
@@ -50,7 +45,7 @@ gomo_out <- read.table(textConnection(all_lines), header = TRUE, sep = "\t")
 enrich_go_raw_out <- paste0(out_folder, "/cluster", cluster_num, "enrich_go_raw.txt")
 write.table(gomo_out, enrich_go_raw_out, sep = '\t', row.names = FALSE, quote = FALSE)
 
-######################## 制作enrich_go.txt ############################
+######################## make enrich_go.txt ############################
 gomo_out <- data.frame(lapply(gomo_out, as.character), stringsAsFactors = FALSE)
 gomo_out <- data.frame(lapply(gomo_out, trimws), stringsAsFactors = FALSE)
 gomo_out <- gomo_out[gomo_out$implied == implied_c, ]
@@ -84,15 +79,14 @@ a <- ggplot(data = plot_df, aes(x = Description, y = Count, fill = ONTOLOGY)) +
   scale_y_continuous(breaks = seq(0, max(plot_df$Count), by = 5), expand = c(0, 0)) +
   theme_bw() +
   theme(
-    # 设置字体为 Times New Roman 和颜色为黑色
     text = element_text(family = "Times New Roman", color = "black"),
-    axis.title.x = element_text(size = 14, color = "black"),  # 调整 x 轴标题的字体大小和颜色
-    axis.title.y = element_text(size = 14, color = "black"),  # 调整 y 轴标题的字体大小和颜色
-    axis.text.x = element_text(size = 12, color = "black"),    # 调整 x 轴刻度标签的字体大小和颜色
-    axis.text.y = element_text(size = 12, color = "black"),    # 调整 y 轴刻度标签的字体大小和颜色
-    panel.grid.major.x = element_line(color = "grey90", linewidth = 0.5),  # 主要 x 轴网格线
-    panel.grid.minor.x = element_blank()  # 隐藏 x 轴的次要网格线
+    axis.title.x = element_text(size = 14, color = "black"), 
+    axis.title.y = element_text(size = 14, color = "black"), 
+    axis.text.x = element_text(size = 12, color = "black"), 
+    axis.text.y = element_text(size = 12, color = "black"),  
+    panel.grid.major.x = element_line(color = "grey90", linewidth = 0.5), 
+    panel.grid.minor.x = element_blank() 
   )
 
 plot_out <- paste0(out_folder, "/cluster", cluster_num, ".png")
-ggsave(a, file = plot_out, width = 9.03, height = 5.74, dpi = 500)
+ggsave(a, file = plot_out, width = 9.03, height = 5.74, dpi = 300)
