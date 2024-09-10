c99faa0bf19c8bb350a0bf13af997532be3b50b6
mofy <141097405+MoFangFFFF@users.noreply.github.com>
2024-09-10 09:20:13 +0800

    Update KmerGOp.sh

--- pipline/KmerGOp/KmerGOp.sh
+++ pipline/KmerGOp/KmerGOp.sh
@@ -18,22 +18,15 @@ fi
 export RANDOM_SEED=12345
 ########################## markerkmer2pep ################################
 bash "$(dirname "$0")/markerkmer2pep.sh" "$markerkmer" "$out_path"
-
 #  markerkmer2pep.sh 生成的输出文件名为 pep.txt
 pep="${out_path}pep.txt"
-
 ########################## tomtom ################################
 Rscript "$(dirname "$0")/tomtom.R" --cluster "$cluster" --markerkmer "$markerkmer" --pep "$pep" --out "$out_path"
-
 ####################  interproscan ####################
 "$interproscan_tool" -i "${out_path}cluster${cluster}_AAseq.fasta" -f tsv -goterms
-
 ####################  GO_Annotations ####################
 Rscript "$(dirname "$0")/GO_Annotations.R" --tsv "${out_path}cluster${cluster}_AAseq.fasta.tsv" --markerkmer "$markerkmer" --cluster "$cluster" --out "$out_path"
-
 
 rm -r temp
 rm "${out_path}cluster${cluster}_AAseq.fasta.tsv" "${out_path}cluster${cluster}_AAseq.fasta"
 rm "${out_path}cluster${cluster}.meme" "$pep" "${out_path}cluster${cluster}.txt"
-
-
