d8f90d014702161382bc0d26e405e5c372116362
mofy <141097405+MoFangFFFF@users.noreply.github.com>
2025-02-08 21:10:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,25 @@ from setuptools import setup
 setup(
     name="mKmer",
     version="1.0.0",
-    packages=['mKmer'],
+    packages=[
+        'mKmer',
+        'mKmer.KmerCell',
+        'mKmer.KmerFrequency',
+        'mKmer.KmerGOn',
+        'mKmer.KmerGOp',
+        'mKmer.KmerRank',
+        'mKmer.RemoveDuplicates',
+        'mKmer.smAnnotation'
+    ],
     entry_points={'console_scripts': ['mKmer = mKmer.cli:main']},
+    include_package_data=True,
+    package_data={
+        'mKmer.KmerCell': ['KmerCell.py'],
+        'mKmer.KmerFrequency': ['KmerFrequency.py'],
+        'mKmer.KmerGOn': ['KmerGOn.py'],
+        'mKmer.KmerGOp': ['KmerGOp.py'],
+        'mKmer.KmerRank': ['KmerRank.py'],
+        'mKmer.RemoveDuplicates': ['RemoveDuplicates.py'],
+        'mKmer.smAnnotation': ['smAnnotation.py'],
+    },
 )
