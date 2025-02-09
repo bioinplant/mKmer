e0f0b0a674466bfe70cf05561a373a160c8c9727
mofy <141097405+MoFangFFFF@users.noreply.github.com>
2025-02-09 14:15:40 +0800

    Update KmerGOn_gomo.R

--- mKmer/KmerGOn/KmerGOn_gomo.R
+++ mKmer/KmerGOn/KmerGOn_gomo.R
@@ -1,9 +1,12 @@
 set.seed(42)
+
+# Load required libraries
 library(optparse)
 library(xml2)
 library(dplyr)
 library(ggplot2)
 
+# Define command-line options
 option_list <- list(
   make_option(c("-c", "--cluster"), type = "integer", help = "Cluster number", metavar = "integer"),
   make_option(c("-o", "--out"), type = "character", help = "Output folder path", metavar = "character"),
@@ -12,65 +15,82 @@ option_list <- list(
   make_option(c("-i", "--implied_c"), type = "character", default = "n", help = "Implied condition", metavar = "character")
 )
 
+# Parse command-line arguments
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
+
 cluster_num <- opt$cluster
 out_folder <- opt$out
 xml_file <- opt$xml
 txt_file <- opt$txt
 implied_c <- opt$implied_c
 
-######################## get information from gomo.xml ############################
-xml_data <- read_xml(xml_file)
-motifs <- xml_data %>% 
-  xml_find_all("//motif")
-results <- character()
+# Read XML data
+gomo_data <- read_xml(xml_file)
+motifs <- xml_find_all(gomo_data, "//motif")
 
+# Extract relevant information from gomo.xml
+results <- character()
 for (motif in motifs) {
-  id <- xml_attr(motif, "id")
-  goterms <- motif %>% xml_find_all("goterm")
+  motif_id <- xml_attr(motif, "id")
+  goterms <- xml_find_all(motif, "goterm")
+  
   for (goterm in goterms) {
     goterm_id <- xml_attr(goterm, "id")
     score <- xml_attr(goterm, "score")
     group <- xml_attr(goterm, "group")
     ontology <- switch(group, "b" = "BP", "m" = "MF", "c" = "CC", group)
-    name <- xml_attr(goterm, "name")
+    description <- xml_attr(goterm, "name")
     implied <- xml_attr(goterm, "implied")
-    results <- c(results, paste(id, "\t", goterm_id, "\t", score, "\t", ontology, "\t", name, "\t", implied))
+    
+    results <- c(results, paste(motif_id, goterm_id, score, ontology, description, implied, sep = "\t"))
   }
 }
 
+# Create data frame from extracted data
 column_names <- "Motif_Identifier\tID\tscore\tONTOLOGY\tDescription\timplied"
 all_lines <- c(column_names, results)
 gomo_out <- read.table(textConnection(all_lines), header = TRUE, sep = "\t")
-enrich_go_raw_out <- paste0(out_folder, "/cluster", cluster_num, "enrich_go_raw.txt")
+
+enrich_go_raw_out <- file.path(out_folder, paste0("cluster", cluster_num, "_enrich_go_raw.txt"))
 write.table(gomo_out, enrich_go_raw_out, sep = '\t', row.names = FALSE, quote = FALSE)
 
-######################## make enrich_go.txt ############################
-gomo_out <- data.frame(lapply(gomo_out, as.character), stringsAsFactors = FALSE)
-gomo_out <- data.frame(lapply(gomo_out, trimws), stringsAsFactors = FALSE)
-gomo_out <- gomo_out[gomo_out$implied == implied_c, ]
-gomo_out <- gomo_out %>% group_by(Motif_Identifier) %>% slice_min(score)
+# Process gomo_out data
+gomo_out <- gomo_out %>%
+  mutate(across(everything(), as.character)) %>%
+  mutate(across(everything(), trimws)) %>%
+  filter(implied == implied_c) %>%
+  group_by(Motif_Identifier) %>%
+  slice_min(score)
 
+# Read cluster.txt file and merge with gomo_out
 tomtom_txt_out <- read.table(txt_file, header = TRUE)
-merge <- left_join(tomtom_txt_out, gomo_out, by = c("best_match_name" = "Motif_Identifier"), relationship = "many-to-many")
-merge <- merge %>% filter(!is.na(implied))
-enrich_go <- merge[, c("ID", "Description", "ONTOLOGY")]
-enrich_go <- enrich_go %>% group_by(ID, Description, ONTOLOGY) %>% summarise(Count = n(), .groups = "drop")
-enrich_go_out <- paste0(out_folder, "/cluster", cluster_num, "enrich_go.txt")
+merged_data <- left_join(tomtom_txt_out, gomo_out, by = c("best_match_name" = "Motif_Identifier"), relationship = "many-to-many") %>%
+  filter(!is.na(implied))
+
+enrich_go <- merged_data %>%
+  select(ID, Description, ONTOLOGY) %>%
+  group_by(ID, Description, ONTOLOGY) %>%
+  summarise(Count = n(), .groups = "drop")
+
+enrich_go_out <- file.path(out_folder, paste0("cluster", cluster_num, "_enrich_go.txt"))
 write.table(enrich_go, enrich_go_out, sep = '\t', row.names = FALSE, quote = FALSE)
 
-######################## plot ############################
-plot_df <- enrich_go
-plot_df <- data.frame(lapply(plot_df, as.character), stringsAsFactors = FALSE)
-plot_df <- data.frame(lapply(plot_df, trimws), stringsAsFactors = FALSE)
+# Prepare data for plotting
+plot_df <- enrich_go %>%
+  mutate(across(everything(), as.character)) %>%
+  mutate(across(everything(), trimws))
+
 plot_df$Count <- as.numeric(plot_df$Count)
 plot_df$ONTOLOGY <- factor(plot_df$ONTOLOGY, levels = c("BP", "CC", "MF"))
 plot_df <- plot_df[order(plot_df$ONTOLOGY, plot_df$Count, plot_df$Description), ]
 plot_df$Description <- factor(plot_df$Description, levels = plot_df$Description)
 
+# Define color scheme
 COLS <- c("#FD8D62", "#8DA1CB", "#66C3A5")
-a <- ggplot(data = plot_df, aes(x = Description, y = Count, fill = ONTOLOGY)) +
+
+# Generate bar plot
+enrich_plot <- ggplot(plot_df, aes(x = Description, y = Count, fill = ONTOLOGY)) +
   geom_bar(stat = "identity", width = 0.8) +
   coord_flip() +
   xlab("GO Term") +
@@ -88,5 +108,6 @@ a <- ggplot(data = plot_df, aes(x = Description, y = Count, fill = ONTOLOGY)) +
     panel.grid.minor.x = element_blank()
   )
 
-plot_out <- paste0(out_folder, "/cluster", cluster_num, ".png")
-ggsave(a, file = plot_out, width = 9.03, height = 5.74, dpi = 500)
+# Save plot
+plot_out <- file.path(out_folder, paste0("cluster", cluster_num, ".png"))
+ggsave(enrich_plot, file = plot_out, width = 9.03, height = 5.74, dpi = 500)
